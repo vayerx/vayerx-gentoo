0ec0eea42699a8be1f93879d647c5261ed2c0d74
Vasiliy Yeremeyev <vayerx@gmail.com>
2019-12-12 23:11:39 +0300

    MR5 setup update: kernel-5.4.2, configs, sets, patches

--- Documentation/screenlayout/dual.sh
+++ Documentation/screenlayout/dual.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 xrandr \
+    --dpi 130 \
     --output eDP1 \
         --primary \
         --mode 1920x1080 \
         --pos 0x1080 \
-        --dpi 157 \
+        --scale 1x1 \
         --rotate normal \
     --output HDMI1 \
         --mode 1920x1080 \
+        --scale 1x1 \
         --pos 0x0 \
-        --dpi 93 \
         --rotate normal \
     --output DP1 \
         --off \
